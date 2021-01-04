7a40d71d6a983a30539c12cd646dff8bb0f99633
eki <eki1490@gmail.com>
2021-01-04 22:10:11 +0700

    change db and move env

--- config/database.js
+++ config/database.js
@@ -3,12 +3,17 @@ require('dotenv').config
 
 
 const makeConnection = () => {
-    
-    const db = mongoose.connect( process.env.MONGODB_URI ,{
+    try{
+    const db = mongoose.connect( process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: false
     });
-    console.log("Database connected to", db);
+    console.log("Mongodb connected", db);
+    } catch (err){
+        console.error(err)
+        process.exit(1)
+    }
 }
 
 module.exports = makeConnection;
