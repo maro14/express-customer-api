784807b44a12b19e24d705e1dc602f3472a50ca5
eki <eki1490@gmail.com>
2021-01-27 20:17:38 +0700

    add middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const userRouter = require('./routes/user');
 const makeConnection = require('./config/database.js');
 const dotenv = require('dotenv');
@@ -10,7 +11,8 @@ dotenv.config({path: '.env'});
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(cors());
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.status(200);
